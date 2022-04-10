104802bf17fc84915016abe581f547520d5804ec
Kristofer Younger <kristofer@Atlantica.local>
2022-04-10 12:02:05 -0400

    checkpoint

--- index.ts
+++ index.ts
@@ -1,15 +1,44 @@
 // Import stylesheets
-import './style.css';
+//import './style.css';
 
 
 const form: HTMLFormElement = document.querySelector('#defineform');
 
+const BASEURL =
+    'https://api.dictionaryapi.dev/api/v2/entries/en/';
+const DEFINEDIV = document.getElementById('definitions');
+
+
 
 form.onsubmit = () => {
-  const formData = new FormData(form);
+  const formData = new FormData(form!);
 
   console.log(formData);
   const text = formData.get('defineword') as string;
-  console.log(text);
+  console.log("on submit", text);
+  const element = document.getElementById('worddefined');
+
+  if (element){
+    element.textContent = text;
+  }
+
+  DEFINEDIV!.innerHTML = '';
+    fetchWordDefinitions(text)
+        .then(defintions => {
+            defintions.forEach(d => {
+                DEFINEDIV!.innerHTML += `<p>${d}</p>`;
+            });
+        })
+        .catch(_ => {
+            DEFINEDIV!.innerHTML += `<p>Error: Unable to find any defintions for ${text}.</p>`;
+        });
   return false; // prevent reload
+};
+
+const fetchWordDefinitions = async text => {
+    const response = await fetch(BASEURL + text);
+    const json = await response.json();
+    return json[0].meanings
+        .flatMap(m => m.definitions)
+        .flatMap(d => d.definition);
 };
